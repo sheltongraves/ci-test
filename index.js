d4a383ec0d15fc66f94ff7a6aa1ead796a6c2fd7
Shelton <shelton@xddh6kdq5m.attlocal.net>
2024-01-24 10:28:44 -0500

    Initial project commit

---
+++ index.js
@@ -0,0 +1,2 @@
+// index.js
+console.log("Hello, CI with GitHub Actions and Docker!");
---
+++ test.js
@@ -0,0 +1,10 @@
+// test.js
+console.log("Running tests...");
+
+// Simple test example
+if (true) { // replace with real tests
+    console.log("Tests passed!");
+} else {
+    console.log("Tests failed!");
+    process.exit(1);
+}
